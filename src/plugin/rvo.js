e451f228536654824988417bd42d1fd1cfd8defe
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-08-01 18:09:27 +0300

    @SilvaTechB

--- src/plugin/rvo.js
+++ src/plugin/rvo.js
@@ -22,7 +22,7 @@ const rvo = async (m, sock) => {
     const type = Object.keys(msg)[0];
     
     const originalCaption = msg[type].caption || '';
-    const newCaption = `${originalCaption}\n\n> © Powered By Ethix-MD`;
+    const newCaption = `${originalCaption}\n\n> © Powered By 🇸​​🇮​​🇱​​🇻​​🇦​ ​🇪​​🇹​​🇭​​🇮​​🇽​-MD`;
 
 
     // Download the media content
